628388d88f0f4c930ce66d3e2b709dd68a8eae88
jongwonIee <ljw3728@gmail.com>
2017-08-13 17:18:43 +0900

    generated essential files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'sessions/new'
+
+  get 'orders/index'
+
+  get 'orders/show'
+
+  get 'orders/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
