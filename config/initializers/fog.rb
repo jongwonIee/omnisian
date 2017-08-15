6078f24689fe251cfc609365f55603696961e405
chae.mingyun <chae.mingyun@nbt.com>
2017-08-15 21:24:57 +0900

    1.fog.rb 에 access_key_id 랑 secret_access_key 추가

--- config/initializers/fog.rb
+++ config/initializers/fog.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     '',                        # required
-      aws_secret_access_key: '',                        # required
+      aws_access_key_id:     'AKIAJSZKSX6XKYTC3U2A',                        # required
+      aws_secret_access_key: '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',                        # required
       region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
       host:                  '',             # optional, defaults to nil
       endpoint:              'https://s3.ap-northeast-2.amazonaws.com' # optional, defaults to nil
